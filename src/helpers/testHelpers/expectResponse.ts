4a8e0b9ea7746542f1295d321c2c003b2bfaca53
samu <1936709+samu@users.noreply.github.com>
2024-11-26 15:21:25 +0100

    Update expectResponse.ts

--- src/helpers/testHelpers/expectResponse.ts
+++ src/helpers/testHelpers/expectResponse.ts
@@ -1,6 +1,7 @@
 import { expect } from "vitest";
 import {
   BasePipelineContextResponse,
+  BasePipelineContextResponsePartiallySet,
   BasePipelineContextResponseSet,
 } from "../../createPipeline/helpers/types";
 
@@ -11,15 +12,21 @@ export function expectResponse<
   current: BasePipelineContextResponse,
   ...expected: Type extends "unset"
     ? []
+    : Type extends "partially-set"
+    ? [Omit<BasePipelineContextResponsePartiallySet, "type">]
     : [Omit<BasePipelineContextResponseSet, "type">]
 ) {
   switch (type) {
-    case "set": {
-      expect(current).toEqual({ ...expected.at(0), type: "set" });
-      return;
-    }
     case "unset": {
       expect(current).toEqual({ type: "unset" });
+      return;
+    }
+    case "partially-set": {
+      expect(current).toEqual({ type: "unset" });
+      return;
+    }
+    case "set": {
+      expect(current).toEqual({ ...expected.at(0), type: "set" });
       return;
     }
   }
